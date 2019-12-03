355ccc66ddaebfb4e6de71ac40f44bb782bedc10
ZEESHAN KHAN <zeeshan.maths@gmail.com>
2019-12-03 12:26:19 +0530

    spring boot main() class

---
+++ src/main/java/com/zeeshan/SpringBootJpaH2CrudApplication.java
@@ -0,0 +1,12 @@
+package com.zeeshan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJpaH2CrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJpaH2CrudApplication.class, args);
+	}
+}
