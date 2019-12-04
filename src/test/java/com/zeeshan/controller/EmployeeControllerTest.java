b9104b3538fcd2ac20d9ef3468ddfea6a7531ab6
ZEESHAN KHAN <zeeshan.maths@gmail.com>
2019-12-04 17:13:05 +0530

    more junit test cases added for EmployeeControllerTest class

--- src/test/java/com/zeeshan/controller/EmployeeControllerTest.java
+++ src/test/java/com/zeeshan/controller/EmployeeControllerTest.java
@@ -3,6 +3,9 @@ package com.zeeshan.controller;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertEquals;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
@@ -10,7 +13,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
-import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -67,13 +69,74 @@ public class EmployeeControllerTest {
 
 		Mockito.when(employeeService.getEmployee(Mockito.anyInt())).thenReturn(emp);
 
-		RequestBuilder requestBuilder = MockMvcRequestBuilders.get(URI).accept("application/json");
+		RequestBuilder requestBuilder = MockMvcRequestBuilders.get(URI).accept("application/json")
+				.contentType("application/json");
 		MvcResult mvcResult = mockMvc.perform(requestBuilder).andReturn();
 
 		String expectedJson = this.mapToJson(emp);
 		String outputInJson = mvcResult.getResponse().getContentAsString();
-		assertThat(outputInJson).isEqualTo(expectedJson);
 
+		assertThat(outputInJson).isEqualTo(expectedJson);
+		assertEquals(HttpStatus.FOUND.value(), mvcResult.getResponse().getStatus());
+
+	}
+
+	@Test
+	public void getAllEmployee() throws Exception {
+
+		Employee emp1 = new Employee(1, "Allu Arjun", "Actor", 456930.50);
+		Employee emp2 = new Employee(2, "Dharmendra", "Actor", 56230.60);
+
+		List<Employee> listEmp = new ArrayList<>();
+		listEmp.add(emp1);
+		listEmp.add(emp2);
+
+		String URI = "/get";
+
+		Mockito.when(employeeService.getAllEmployee()).thenReturn(listEmp);
+
+		RequestBuilder requestBuilder = MockMvcRequestBuilders.get(URI).accept("application/json")
+				.contentType("application/json");
+
+		MvcResult mvcResult = mockMvc.perform(requestBuilder).andReturn();
+
+		String expectedJson = this.mapToJson(listEmp);
+		String outputInJson = mvcResult.getResponse().getContentAsString();
+
+		assertThat(outputInJson).isEqualTo(expectedJson);
+		assertEquals(HttpStatus.OK.value(), mvcResult.getResponse().getStatus());
+
+	}
+
+	@Test
+	public void testUpdateEmployee() throws Exception {
+
+		Employee emp = new Employee(1, "Krishna", "Network Engineer", 56230.40);
+
+		String URI = "/update/1";
+		String inputInJson = this.mapToJson(emp);
+		RequestBuilder requestBuilder = MockMvcRequestBuilders.put(URI).accept("application/json").content(inputInJson)
+				.contentType("application/json");
+		emp.setDesg("Java Developer");
+
+		Mockito.when(employeeService.updateEmployee(Mockito.any(Employee.class), Mockito.anyInt())).thenReturn(emp);
+
+		MvcResult mvcResult = mockMvc.perform(requestBuilder).andReturn();
+		String expectedJson = this.mapToJson(emp);
+		String outputInJson = mvcResult.getResponse().getContentAsString();
+		assertThat(expectedJson).isEqualTo(outputInJson);
+		assertEquals(HttpStatus.OK.value(), mvcResult.getResponse().getStatus());
+	}
+
+	@Test
+	public void testDeleteEmployee() throws Exception {
+
+		Employee emp = new Employee(1, "Rajesh", "Java Developer", 456203.40);
+		String URI = "/delete/1";
+
+		RequestBuilder requestBuilder = MockMvcRequestBuilders.delete(URI).contentType("text/plain");
+		MvcResult mvcResult = mockMvc.perform(requestBuilder).andReturn();
+		assertEquals(HttpStatus.NO_CONTENT.value(), mvcResult.getResponse().getStatus());
 	}
 
 	private String mapToJson(Object obj) throws JsonProcessingException {
